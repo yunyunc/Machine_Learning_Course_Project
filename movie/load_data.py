b1777bd6a251ae017581fff0ddc1f685c0d363db
Zilun Peng <zilunpeng@gmail.com>
2017-11-20 10:07:57 -0500

    load occ

--- movie/load_data.py
+++ movie/load_data.py
@@ -6,7 +6,6 @@ class load_data:
     def __init__(self, name):
         self.name = name
 
-
     def load_user_info(self):
         if self.name=='100k':
             user_info_file_name = 'data/movie/ml-100k/u.user'
@@ -22,24 +21,30 @@ class load_data:
         age = int(line[1])
         age = self.parseAge(age)
         gen = [1, 0] if line[2] == 'M' else [0, 1]
-        return line
+        occ = self.parseOcc(line[3])
+        return age+gen+occ
 
     def parseAge(self,age):
-        age_array = np.zeros(5)
         if age<25:
-            age_array[0] = 1
+            age_array = [1,0,0,0,0]
         elif age>25 and age <35:
-            age_array[1] = 1;
+            age_array = [0,1,0,0,0]
         elif age>35 and age<45:
-            age_array[2] = 1
+            age_array = [0,0,1,0,0]
         elif age>45 and age<55:
-            age_array[3] = 1
-        else: age_array[4] = 1
+            age_array = [0,0,0,1,0]
+        else: age_array = [0,0,0,0,1]
         return age_array
 
-
-
+    def parseOcc(self,occ):
+        occupations = ['administrator','artist','doctor','educator','engineer','entertainment','executive','healthcare','homemaker',
+                       'lawyer','librarian','marketing','none','other','programmer','retired','salesman','scientist','student',
+                       'technician','writer']
+        occs = np.zeros(len(occupations))
+        ind = occupations.index(occ)
+        if  ind>= 0:
+            occs[ind] = 1
+        return list(occs.astype(int))
 
 test = load_data('100k')
 info = test.load_user_info()
-print('haha')
