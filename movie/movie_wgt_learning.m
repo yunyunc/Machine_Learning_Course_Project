61b8da8fe48feb1226d4bf7638cc037931644496
Zilun Peng <zilunpeng@gmail.com>
2017-11-29 22:48:14 -0500

    GOT MY WEIGHTS!! HAHAHAHAHA

--- movie/movie_wgt_learning.m
+++ movie/movie_wgt_learning.m
@@ -29,6 +29,29 @@ nEdges = edgeStruct.nEdges;
 %%Training
 
 % Make nodeMap
-nodeMap = zeros(n_nodes,maxState,'int32');
+% nodeMap = zeros(n_nodes,maxState,'int32');
+% num_wgts = 0;
+% for i=1:n_nodes
+%     num_states = nStates(i);
+%     for j=1:num_states
+%         nodeMap(i,j) = num_wgts+j;
+%     end
+%     num_wgts = num_wgts + num_states;
+% end
+% 
+ising = 0; % Use full potentials
+tied = 0; % Each node/edge has its own parameters
+[nodeMap,edgeMap] = UGM_makeMRFmaps(n_nodes,edgeStruct,ising,tied);
+nParams = max([nodeMap(:);edgeMap(:)])
+w = zeros(nParams,1);
+
+% Compute sufficient statistics
+suffStat = UGM_MRF_computeSuffStat(data,nodeMap,edgeMap,edgeStruct);
+
+% Evaluate NLL
+nll = UGM_MRF_NLL(w,n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_Tree)
+
+% Optimize
+w = minFunc(@UGM_MRF_NLL,w,[],n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_Tree);
 
 
