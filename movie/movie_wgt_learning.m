cb49e380d63402135d2572204b8a7b739a7e098f
Zilun Peng <zilunpeng@gmail.com>
2017-12-05 17:11:34 -0500

    delete redundant lines

--- movie/movie_wgt_learning.m
+++ movie/movie_wgt_learning.m
@@ -27,18 +27,6 @@ maxState = max(nStates);
 nEdges = edgeStruct.nEdges;
 
 %%Training
-
-% Make nodeMap
-% nodeMap = zeros(n_nodes,maxState,'int32');
-% num_wgts = 0;
-% for i=1:n_nodes
-%     num_states = nStates(i);
-%     for j=1:num_states
-%         nodeMap(i,j) = num_wgts+j;
-%     end
-%     num_wgts = num_wgts + num_states;
-% end
-% 
 ising = 0; % Use full potentials
 tied = 0; % Each node/edge has its own parameters
 [nodeMap,edgeMap] = UGM_makeMRFmaps(n_nodes,edgeStruct,ising,tied);
@@ -49,9 +37,9 @@ w = zeros(nParams,1);
 suffStat = UGM_MRF_computeSuffStat(data,nodeMap,edgeMap,edgeStruct);
 
 % Evaluate NLL
-nll = UGM_MRF_NLL(w,n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_MeanField)
+nll = UGM_MRF_NLL(w,n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_Tree)
 
 % Optimize
-w = minFunc(@UGM_MRF_NLL,w,[],n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_MeanField);
+w = minFunc(@UGM_MRF_NLL,w,[],n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_Tree);
 
 
