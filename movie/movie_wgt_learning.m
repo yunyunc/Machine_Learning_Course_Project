6c8a3357dbddd4543872354bd88fb0be133a6d65
Zilun Peng <zilunpeng@gmail.com>
2017-12-20 15:45:45 -0500

    learning wgts for model 1.5

--- movie/movie_wgt_learning.m
+++ movie/movie_wgt_learning.m
@@ -5,23 +5,28 @@ cd UGM
 addpath(genpath(pwd))
 cd ..
 
-%%load data
+%%Load data
 load tr_data.mat
-data = zeros(length(FrameStack),24);
-for i=1:length(FrameStack)
-    data(i,:) = FrameStack{i};
+data = zeros(length(FrameStack)/80,57);
+for i=1:length(FrameStack)/80
+    data_i = FrameStack{i};
+    age_one_hot = ones(1,5);
+    age_one_hot(data_i(1)) = 2;
+    occ_one_hot = ones(1,21);
+    occ_one_hot(data_i(3)) = 2;
+    year_one_hot = ones(1,10);
+    year_one_hot(data_i(4)) = 2;
+    data(i,:) = [age_one_hot data_i(2) occ_one_hot year_one_hot data_i(5:23) data_i(25)]; %ignore movie id for now
 end
 clear FrameStack
-n_instances = length(data);
-n_nodes = 24;
+[nInstances, n_nodes] = size(data);
+
+%%Load adj from memory
+load learned_ugm_1.5_1000_trIns.mat
+adj = adjFinal;
 
 %%Make edgeStruct
 nStates = max(data);
-adj = zeros(n_nodes);
-for i=1:n_nodes-1
-    adj(i,n_nodes) = 1;
-end
-adj = adj+adj';
 edgeStruct = UGM_makeEdgeStruct(adj,nStates,0,100); %no mex
 maxState = max(nStates);
 nEdges = edgeStruct.nEdges;
@@ -37,9 +42,10 @@ w = zeros(nParams,1);
 suffStat = UGM_MRF_computeSuffStat(data,nodeMap,edgeMap,edgeStruct);
 
 % Evaluate NLL
-nll = UGM_MRF_NLL(w,n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_Tree)
+inferFunc = @UGM_Infer_MeanField;
+nll = UGM_MRF_NLL(w,nInstances,suffStat,nodeMap,edgeMap,edgeStruct,inferFunc)
 
 % Optimize
-w = minFunc(@UGM_MRF_NLL,w,[],n_instances,suffStat,nodeMap,edgeMap,edgeStruct,@UGM_Infer_LBP);
+w = minFunc(@UGM_MRF_NLL,w,[],nInstances,suffStat,nodeMap,edgeMap,edgeStruct,inferFunc);
 
 [nodePot,edgePot] = UGM_MRF_makePotentials(w,nodeMap,edgeMap,edgeStruct);
